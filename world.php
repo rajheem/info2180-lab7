c5e722ff145f486bb9f809d6d84d27d52cee49d2
Rajheem <rajheem1@gmail.com>
2019-11-20 16:48:32 +0000

    Updates

--- world.php
+++ world.php
@@ -6,11 +6,10 @@ $dbname = 'world';
 $country=$_GET['country'];
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<?php if(!$_GET):?>
+<?php if(!$_GET||$country===''):?>
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
@@ -20,10 +19,29 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   filter_var($country, FILTER_SANITIZE_STRING);
   foreach ($results as $row):
     if ($row['name']===$country):?>
-      <h3><?= $row['name'];?></h3>
-      <h3><?= $row['head_of_state'];?></h3>
+       <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+        <table>
+            <caption>List of Countries</caption>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Continent</th>
+                    <th>Independence</th>
+                    <th>Head of State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><?= $row['name']; ?></td>
+                    <td><?= $row['continent']; ?></td>
+                    <td><?= $row['independence_year']; ?></td>
+                    <td><?= $row['head_of_state']; ?></td>
+                </tr>
+            </tbody>
+        </table>
       <?php
     endif;
   endforeach;
 endif;
+
 ?>
